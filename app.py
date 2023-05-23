e83cbde1cce606ed581e13508338ed760e8f5914
Chickychicken <112368800+Chickychicken@users.noreply.github.com>
2023-05-23 12:46:53 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,57 @@
 import streamlit as st
+import pandas as pd
+from matplotlib import pyplot as plt
+import io
 
-st.title('Title of my application')
+web_apps = st.sidebar.selectbox("Select Web Apps",
+                                ("Exploratory Data Analysis", "Distributions"))
 
-st.markdown('this is **bold text')
 
-agree = st.checkbox('I agree')
+if web_apps == "Exploratory Data Analysis":
 
-if agree:
-    st.write('Great!')
-    st.markdown('This is markdown **text**')
+  uploaded_file = st.sidebar.file_uploader("Choose a file")
 
-st.sidebar.title('title of sidebar')
-st.sidebar.markdown('Markdown **text** in the *sidebar*')
+  if uploaded_file is not None:
+    # Can be used wherever a "file-like" object is accepted:
+    df = pd.read_csv(uploaded_file)
+    show_df = st.checkbox("Show Data Frame", key="disabled")
 
-agree_sidebar = st.sidebar.checkbox('side bar check')
+    if show_df:
+      st.write(df)
 
-if agree_sidebar:
-    st.sidebar.write('sidebar checked')
+    column_type = st.sidebar.selectbox('Select Data Type',
+                                       ("Numerical", "Categorical", "Bool", "Date"))
+
+    if column_type == "Numerical":
+      numerical_column = st.sidebar.selectbox(
+          'Select a Column', df.select_dtypes(include=['int64', 'float64']).columns)
+
+      # histogram
+      choose_color = st.color_picker('Pick a Color', "#69b3a2")
+      choose_opacity = st.slider(
+          'Color Opacity', min_value=0.0, max_value=1.0, step=0.05)
+
+      hist_bins = st.slider('Number of bins', min_value=5,
+                            max_value=150, value=30)
+      hist_title = st.text_input('Set Title', 'Histogram')
+      hist_xtitle = st.text_input('Set x-axis Title', numerical_column)
+
+      fig, ax = plt.subplots()
+      ax.hist(df[numerical_column], bins=hist_bins,
+              edgecolor="black", color=choose_color, alpha=choose_opacity)
+      ax.set_title(hist_title)
+      ax.set_xlabel(hist_xtitle)
+      ax.set_ylabel('Count')
+
+      st.pyplot(fig)
+      filename = "plot.png"
+      fig.savefig(filename,dpi = 300)
+
+      # Display the download button
+      with open("plot.png", "rb") as file:
+        btn = st.download_button(
+            label="Download image",
+            data=file,
+            file_name="flower.png",
+            mime="image/png"
+        )
