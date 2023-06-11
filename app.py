80eb6fac64a70a7181d8b0ae3ea68011c8c469e9
Chickychicken <112368800+Chickychicken@users.noreply.github.com>
2023-06-10 18:38:42 -0700

    Updated app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 from matplotlib import pyplot as plt
-import io 
+import io
 
 web_apps = st.sidebar.selectbox("Select Web Apps",
                                 ("Exploratory Data Analysis", "Distributions"))
@@ -9,49 +9,73 @@ web_apps = st.sidebar.selectbox("Select Web Apps",
 
 if web_apps == "Exploratory Data Analysis":
 
-  uploaded_file = st.sidebar.file_uploader("Choose a file")
+    uploaded_file = st.sidebar.file_uploader("Choose a file")
 
-  if uploaded_file is not None:
-    # Can be used wherever a "file-like" object is accepted:
-    df = pd.read_csv(uploaded_file)
-    show_df = st.checkbox("Show Data Frame", key="disabled")
+    if uploaded_file is not None:
+        df = pd.read_csv(uploaded_file)
+        show_df = st.checkbox("Show Data Frame", key="disabled")
 
-    if show_df:
-      st.write(df)
+        if show_df:
+            st.write(df)
 
-    column_type = st.sidebar.selectbox('Select Data Type',
-                                       ("Numerical", "Categorical", "Bool", "Date"))
+        chart_type = st.sidebar.selectbox(
+            'Select Chart Type', ("Line Chart", "Histogram"))
+        
+        if chart_type == "Line Chart":
+            numerical_column = st.sidebar.selectbox(
+                'Select a Numerical Column', df.select_dtypes(include=['int64', 'float64']).columns)
+            categorical_column = st.sidebar.selectbox(
+                'Select a Categorical Column', df.select_dtypes(include=['object', 'category']).columns)
 
-    if column_type == "Numerical":
-      numerical_column = st.sidebar.selectbox(
-          'Select a Column', df.select_dtypes(include=['int64', 'float64']).columns)
+            line_title = st.text_input('Set Title', 'Line Chart')
+            line_xtitle = st.text_input('Set x-axis Title', categorical_column)
+            line_ytitle = st.text_input('Set y-axis Title', numerical_column)
 
-      # histogram
-      choose_color = st.color_picker('Pick a Color', "#69b3a2")
-      choose_opacity = st.slider(
-          'Color Opacity', min_value=0.0, max_value=1.0, step=0.05)
+            fig, ax = plt.subplots()
+            ax.plot(df[categorical_column], df[numerical_column])
+            ax.set_title(line_title)
+            ax.set_xlabel(line_xtitle)
+            ax.set_ylabel(line_ytitle)
 
-      hist_bins = st.slider('Number of bins', min_value=5,
-                            max_value=150, value=30)
-      hist_title = st.text_input('Set Title', 'Histogram')
-      hist_xtitle = st.text_input('Set x-axis Title', numerical_column)
+            st.pyplot(fig)
+            filename = "line_plot.png"
+            fig.savefig(filename, dpi=300)
+            with open("line_plot.png", "rb") as file:
+                btn = st.download_button(
+                    label="Download image",
+                    data=file,
+                    file_name="line_plot.png",
+                    mime="image/png"
 
-      fig, ax = plt.subplots()
-      ax.hist(df[numerical_column], bins=hist_bins,
-              edgecolor="black", color=choose_color, alpha=choose_opacity)
-      ax.set_title(hist_title)
-      ax.set_xlabel(hist_xtitle)
-      ax.set_ylabel('Count')
+                )
+        elif chart_type == "Histogram":
+            numerical_column = st.sidebar.selectbox(
+                'Select a Column', df.select_dtypes(include=['int64', 'float64']).columns)
 
-      st.pyplot(fig)
-      filename = "plot.png"
-      fig.savefig(filename,dpi = 300)
+            choose_color = st.color_picker('Pick a Color', "#69b3a2")
+            choose_opacity = st.slider(
+                'Color Opacity', min_value=0.0, max_value=1.0, step=0.05)
 
-      # Display the download button
-      with open("plot.png", "rb") as file:
-        btn = st.download_button(
-            label="Download image",
-            data=file,
-            file_name="flower.png",
-            mime="image/png"
-        )
+            hist_bins = st.slider('Number of bins', min_value=5,
+                                  max_value=150, value=30)
+            hist_title = st.text_input('Set Title', 'Histogram')
+            hist_xtitle = st.text_input('Set x-axis Title', numerical_column)
+
+            fig, ax = plt.subplots()
+            ax.hist(df[numerical_column], bins=hist_bins,
+                    edgecolor="black", color=choose_color, alpha=choose_opacity)
+            ax.set_title(hist_title)
+            ax.set_xlabel(hist_xtitle)
+            ax.set_ylabel('Count')
+
+            st.pyplot(fig)
+            filename = "plot.png"
+            fig.savefig(filename, dpi=300)
+
+            with open("plot.png", "rb") as file:
+                btn = st.download_button(
+                    label="Download image",
+                    data=file,
+                    file_name="plot.png",
+                    mime="image/png"
+                )
